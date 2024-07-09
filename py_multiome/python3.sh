f11ff31f403058531095ec54a04b6a210048db61
AmosFong1 <amoskwoklumfong@gmail.com>
2024-07-08 19:31:16 -0700

    removed py_scenicplus

--- py_multiome/python3.sh
+++ py_multiome/python3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE=/projects/dscott_prj/amfong/multiome_dz/images/py_scenicplus_3.11.sif
+IMAGE=/projects/dscott_prj/amfong/multiome_dz/images/py_multiome_3.11.sif
 HOME=/home/amfong
 SCRIPT=/projects/dscott_prj/amfong/multiome_dz/scratch/python3.py
 
