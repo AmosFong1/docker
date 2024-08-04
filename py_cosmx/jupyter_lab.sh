b4e8471a0e33238c03bf132750fb53da0efe2653
AmosFong1 <amoskwoklumfong@gmail.com>
2024-08-03 21:18:54 -0700

    py_cosmx scanpy

---
+++ py_cosmx/jupyter_lab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IMAGE=/projects/dscott_prj/amfong/cosmx_dz/images/py_cosmx_3.11.sif
+HOME=/home/amfong
+PORT=9079
+export APPTAINERENV_JUPYTER_TOKEN='famousamos'
+
+singularity exec \
+    --home=${HOME} \
+    --bind=/home/amfong \
+    --bind=/projects/dscott_prj/amfong \
+    --bind=/projects/dscott_scratch/amfong \
+    --bind=/tmp \
+    ${IMAGE} jupyter lab \
+    --port=${PORT} \
+    --no-browser \
+    --notebook-dir=${HOME}
