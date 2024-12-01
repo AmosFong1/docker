8704faaa00b36f1c5482aa7e676e9c50aa27d321
AmosFong1 <amoskwoklumfong@gmail.com>
2024-12-01 02:22:57 -0800

    archr 1.0.3

--- py_cosmx/jupyter_lab.sh
+++ py_cosmx/jupyter_lab.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-IMAGE=/projects/dscott_prj/amfong/cosmx_dz/images/py_cosmx_3.11.sif
-HOME=/home/amfong
-PORT=9079
-export APPTAINERENV_JUPYTER_TOKEN='famousamos'
+IMAGE="/projects/dscott_prj/amfong/cosmx_dz/images/py_cosmx_3.11.sif"
+HOME="/home/amfong"
+PORT=7890
 
 singularity exec \
-    --home=${HOME} \
-    --bind=/home/amfong \
-    --bind=/projects/dscott_prj/amfong \
-    --bind=/projects/dscott_scratch/amfong \
-    --bind=/tmp \
-    ${IMAGE} jupyter lab \
+    --home="${HOME}" \
+    --bind="/home/amfong" \
+    --bind="/projects/dscott_prj/amfong" \
+    --bind="/projects/dscott_scratch/amfong" \
+    --bind="/tmp" \
+    "${IMAGE}" jupyter lab \
     --port=${PORT} \
     --no-browser \
-    --notebook-dir=${HOME}
+    --notebook-dir="${HOME}"
--- py_multiome/jupyter_lab.sh
+++ py_multiome/jupyter_lab.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-IMAGE=/projects/dscott_prj/amfong/multiome_dz/images/py_multiome_3.11.sif
-HOME=/home/amfong
+IMAGE="/projects/dscott_prj/amfong/cosmx_dz/images/py_multiome_3.11.sif"
+HOME="/home/amfong"
 PORT=9079
-export APPTAINERENV_JUPYTER_TOKEN='famousamos'
 
 singularity exec \
-    --home=${HOME} \
-    --bind=/home/amfong \
-    --bind=/projects/dscott_prj/amfong \
-    --bind=/projects/dscott_scratch/amfong \
-    --bind=/tmp \
-    ${IMAGE} jupyter lab \
+    --home="${HOME}" \
+    --bind="/home/amfong" \
+    --bind="/projects/dscott_prj/amfong" \
+    --bind="/projects/dscott_scratch/amfong" \
+    --bind="/tmp" \
+    "${IMAGE}" jupyter lab \
     --port=${PORT} \
     --no-browser \
-    --notebook-dir=${HOME}
+    --notebook-dir="${HOME}"
