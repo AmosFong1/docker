13b5dfa374fe9dd019cd9eb50554b6d9d357d550
AmosFong1 <amoskwoklumfong@gmail.com>
2025-11-07 14:32:49 -0800

    survminer

---
+++ r_cosmx/rserver.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+IMAGE="/projects/dscott_prj/amfong/cosmx_dz/images/r_cosmx_4.3.3.sif"
+HOME="/home/amfong"
+PORT=8030
+export PASSWORD="famousamos"
+export TMPDIR="/home/amfong"
+
+singularity exec \
+    --bind="${TMPDIR}:/var/run/" \
+    --bind="${TMPDIR}:/var/lib/rstudio-server" \
+    --home="${HOME}" \
+    --bind="/home/amfong" \
+    --bind="/projects/dscott_prj/" \
+    --bind="/projects/dscott_scratch/" \
+    --bind="/projects/rmorin" \
+    --bind="/projects/rmorin_scratch" \
+    --bind="/projects/clc" \
+    --bind="/tmp" \
+    "${IMAGE}" "/usr/lib/rstudio-server/bin/rserver" \
+    --auth-none=0 \
+    --auth-pam-helper-path=pam-helper \
+    --www-port=${PORT} \
+    --server-user="${USER}" \
+    --auth-timeout-minutes=0 \
+    --auth-stay-signed-in-days=30
